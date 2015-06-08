818483262fe31859212110731aef32916212cde7
Carlos Cunha <ccfontes@gmail.com>
2015-06-08 09:42:00 +0100

    Initial commit

---
+++ project.clj
@@ -0,0 +1,16 @@
+(defproject the-flood "0.1.0"
+
+  :description "Clojure Library for the Flood Fill Algorithm"
+
+  :url "https://github.com/ccfontes/the-flood"
+
+  :license {:name "Apache License, Version 2.0"
+            :url "https://www.apache.org/licenses/LICENSE-2.0"}
+
+  :scm {:name "git"
+        :url "https://github.com/ccfontes/the-flood"}
+
+  :repl-options {:init-ns the-flood.core}
+
+  :dependencies [[org.clojure/clojure "1.5.1"]])
+
---
+++ src/the_flood/core.clj
@@ -0,0 +1,49 @@
+(ns the-flood.core
+  (:require [clojure.string :as str]))
+
+(defn next-frontier [img [w h] [x y] source-char fill-char]
+  (for [i [(dec y) y (inc y)]
+        j [(dec x) x (inc x)]
+        :when (and (>= i 0)
+                   (>= j 0)
+                   (< i h)
+                   (< j w)
+                   (or (= x j) (= y i))
+                   (not (and (= x j) (= y i)))
+                   (= source-char (get-in img [i j])))]
+    [[j i] source-char fill-char]))
+
+(defn next-frontier-wrap [img [w h] [x y] source-char fill-char]
+  (for [i [(if (>= (dec y) 0) (dec y) (dec h))
+           y
+           (if (< (inc y) h) (inc y) 0)]
+        j [(if (>= (dec x) 0) (dec x) (dec w))
+           x
+           (if (< (inc x) w) (inc x) 0)]
+        :when (and (or (= x j) (= y i))
+                   (not (and (= x j) (= y i)))
+                   (= source-char (get-in img [i j])))]
+    [[j i] source-char fill-char]))
+
+(defn -flood-fill [img frontier wrap]
+  (if (-> frontier count zero?)
+    img
+    (let [front (first frontier)
+          x (ffirst front)
+          y (-> front first second)
+          source-char (second front)
+          fill-char (last front)
+          w (-> img first count)
+          h (count img)]
+      (recur
+        (assoc-in img [y x] fill-char)
+        (if wrap
+          (concat (next frontier)
+          	      (next-frontier-wrap img [w h] [x y] source-char fill-char))
+          (concat (next frontier)
+          	      (next-frontier img [w h] [x y] source-char fill-char)))
+        wrap))))
+
+(defn flood-fill [img [x y] fill-char wrap]
+  (let [source-char (get-in img [y x])]
+    (-flood-fill img [[[x y] source-char fill-char]] wrap)))
